0890a5c3aa873480d932962ec62b031d6a4250b0
Trevan Reese <trevanreeser41@gmail.com>
2020-02-12 12:44:13 -0700

    initialize repo

---
+++ dashboard.js
@@ -0,0 +1,7 @@
+class Scoreboard {
+    // populateScores = () => {
+    //     fetch("http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard")
+    //     .then(response => response.json())
+    //     response.
+    // }
+}
