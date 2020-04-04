2224ba1d8f56e2392d6711d961c708653cccd5ff
Trevan Reese <trevanreeser41@gmail.com>
2020-04-03 20:48:49 -0600

    clean up rankings console errors

--- scoreboard/src/Rankings.js
+++ scoreboard/src/Rankings.js
@@ -22,9 +22,9 @@ export class Rankings extends Component {
 
     populateRankings = () => { 
         var dataarray =[]
-        console.log(this.state.sports.length)
+        //console.log(this.state.sports.length)
         for (let index = 0; index < this.state.sports.length; index++) {
-            console.log(this.state.sports[index])
+            //console.log(this.state.sports[index])
             fetch(`http://site.api.espn.com/apis/site/v2/sports/${this.state.sports[index][0]}/${this.state.sports[index][1]}/rankings`)
                 .then(function (response) {
                     if (response.ok) {
@@ -34,38 +34,38 @@ export class Rankings extends Component {
                     throw new Error("Unable to retrieve required data from server.");
                 })
                 .then(data => {
-                    console.log(data)
+                    //console.log(data)
                     dataarray.push(data)
                     this.setState({loading: false,data: dataarray})
-                    console.log(this.state.data)
+                    //console.log(this.state.data)
                     //console.log(this.state.data)
                 })
                 .catch(function (error) {
                 console.log("Error: ", error.message);
             });
             }   
-            console.log(this.state.data)
+            // console.log(this.state.data)
     }
 
     render() {
         if (this.state.loading===false){
             var newData = []
-            console.log("THIS IS DATA:")
-            console.log(this.state.data)
+            // console.log("THIS IS DATA:")
+            // console.log(this.state.data)
             for (let index = 0; index < this.state.data.length; index++) {
-                console.log(index)
+                //console.log(index)
                 var tableData = []
                 for (let index1 = 0; index1 < this.state.data[index].rankings[0].ranks.length; index1++) {
                 
                     tableData.push(
                         <tr>
-                            <td id="logo"><img id="thumb" alt="logo" src={this.state.data[index].rankings[0].ranks[index1].team.logo}/></td><td class="rank">{this.state.data[index].rankings[0].ranks[index1].current}</td><td> {this.state.data[index].rankings[0].ranks[index1].team.location} {this.state.data[index].rankings[0].ranks[index1].team.name}</td>
+                            <td id="logo"><img id="thumb" alt="logo" src={this.state.data[index].rankings[0].ranks[index1].team.logo}/></td><td className="rank">{this.state.data[index].rankings[0].ranks[index1].current}</td><td> {this.state.data[index].rankings[0].ranks[index1].team.location} {this.state.data[index].rankings[0].ranks[index1].team.name}</td>
                         </tr>
                     )   
                 }
                 newData.push(
-                    <div class="grid-item rankings">
-                        <table class="rankingsTable">
+                    <div className="grid-item rankings">
+                        <table className="rankingsTable">
                             <thead><tr><th colSpan="3">{this.state.data[index].leagues[0].name} {this.state.data[index].rankings[0].name}</th></tr></thead>
                             <tbody>{tableData}</tbody>
                         </table>
@@ -75,7 +75,7 @@ export class Rankings extends Component {
             }
 
             return(
-                <span class="grid-container">
+                <span className="grid-container">
                     {newData}
                 </span>
             )
