428526ec42325ea8cf73b0d1712727d8bfc7c8ec
Jason <jason.iowa@gmail.com>
2020-04-08 19:01:40 -0500

    added build files to repo

---
+++ scoreboard/build/precache-manifest.70ac3e1da78648fed237acd5ab2f73bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d534db248997e7054eaf9f0e88797905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb6e3826c69415f5bd11",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "8869d168069fcddd0522",
+    "url": "/static/css/main.f676c05b.chunk.css"
+  },
+  {
+    "revision": "cb6e3826c69415f5bd11",
+    "url": "/static/js/2.cbfdba64.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.cbfdba64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8869d168069fcddd0522",
+    "url": "/static/js/main.d8c77822.chunk.js"
+  },
+  {
+    "revision": "62bb5ea719ececad6e95",
+    "url": "/static/js/runtime-main.0747cf82.js"
+  }
+]);
