1f35888adfb639b4b805f338c7283f5c689ad639
Jason <jason.iowa@gmail.com>
2020-04-14 00:48:14 -0500

    add build

---
+++ scoreboard/build/precache-manifest.d68b3c08f4cc73a979e378751ba130d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ab1681028e7d953cceb5c5c673712e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a795845e65d6d2cf56e9",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "34c5ea3da118cdc29eea",
+    "url": "/static/css/main.49248098.chunk.css"
+  },
+  {
+    "revision": "a795845e65d6d2cf56e9",
+    "url": "/static/js/2.5ad653af.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.5ad653af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c5ea3da118cdc29eea",
+    "url": "/static/js/main.e71d68c9.chunk.js"
+  },
+  {
+    "revision": "62bb5ea719ececad6e95",
+    "url": "/static/js/runtime-main.0747cf82.js"
+  }
+]);
