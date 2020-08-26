f2306ad4433cc72c72f5fe9952394448c10e2b16
Trevan Reese <trevanreeser41@gmail.com>
2020-08-26 09:20:50 -0600

    rm yml

--- scoreboard/src/index.js
+++ scoreboard/src/index.js
@@ -4,9 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { disableBodyScroll } from 'body-scroll-lock';
+ 
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
+const targetElement = document.querySelector('.background');
+disableBodyScroll(targetElement);
 
 
 ReactDOM.render(<BrowserRouter basename={baseUrl}>
